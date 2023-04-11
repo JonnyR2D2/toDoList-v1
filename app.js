fa007c0cf8ca4fb120f2e98c6909f1ba1535bdf2
JonnyR2D2 <jonathan.m.redmond@gmail.com>
2023-04-11 11:12:28 +0000

    added ejs, got list to work properly

--- app.js
+++ app.js
@@ -3,23 +3,41 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+let Items = [];
+let day = '';
+
+app.use(bodyParser.urlencoded({extended:true}));
+
 app.set('view engine', 'ejs');
+
 
 app.get('/', function(req,res){
 
     let today = new Date();
     let curentDay = today.getDay();
-    let day = ['Monday', 'Tuesday', 'Wedensday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-    let x = '';
+    let options = {
+        weekday: 'long',
+        day: 'numeric',
+        month: 'long',
+    };
 
-
-    day.forEach((day, index)=>{
-        if(index == new Date().getDay()){
-            return x = day;
-        }
-    })
-    res.render('list',{kindOfDay:x});
+    let day = today.toLocaleDateString('en-US', options);
+    res.render('list', {kindOfDay:day, newItem: Items})
 });
+
+
+app.post('/', function(req,res){
+
+    let newItem = req.body.addItem;
+
+    Items.push(newItem);
+
+    console.log(newItem);
+
+    res.redirect('/');
+})
+
+
 
 app.listen(3000, function(){
     console.log('server has started running on port 3000');
